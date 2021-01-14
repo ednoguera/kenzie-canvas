29eb6cafeeb0e9d0549de85719b5cff816bf47c7
Edson Silva <edsonoguera@outlook.com>
2021-01-14 19:07:19 -0300

    feat: added basic router structure to the mvp

--- src/pages/access/index.jsx
+++ src/pages/access/index.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 import SideNav from '../../components/side-bar'
 import ValidateLogin from '../../components/validate-login'
 import BannerLogin from '../../components/banner-login'
-import { Switch, Route, Link } from 'react-router-dom'
 
-const AccessPage = () => {
+const AccessPage = (props) => {
     return (
         <div>
             <SideNav />
