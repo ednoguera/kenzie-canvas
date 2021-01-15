4934d3db0e4c8d865643b969834283187267d4f1
Edson Silva <edsonoguera@outlook.com>
2021-01-15 12:55:04 -0300

    fix: change routes logic

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom'
-import 'antd/dist/antd.css'
+import 'antd/dist/antd.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
