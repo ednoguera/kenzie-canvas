af0aa4486c53f9c37c1207345992ae61f1814e90
Edson Silva <edsonoguera@outlook.com>
2021-01-17 13:18:12 -0300

    feat: props change the modules page state

--- src/components/side-bar_modules/index.jsx
+++ src/components/side-bar_modules/index.jsx
@@ -15,7 +15,7 @@ export default SideBarModules
 
 const SideNav = styled.div`
     height: 100%; /* Full-height: remove this if you want "auto" height */
-    width: 10%; /* Set the width of the sidebar */
+    width: 8%; /* Set the width of the sidebar */
     position: fixed; /* Fixed Sidebar (stay in place on scroll) */
     z-index: 1; /* Stay on top */
     top: 0; /* Stay at the top */
--- src/pages/modules/index.jsx
+++ src/pages/modules/index.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import SideBarModules from '../../components/side-bar-modules/'
+import React, { useState } from 'react'
+import SideBarModules from '../../components/side-bar_modules'
+import Header from '../../components/header_modules/'
 
 const Modules = () => {
+    const [menuModules, setMenuModules] = useState(false)
+    console.log("Estado do módulo:", menuModules)
+
     return (
-        <SideBarModules />
+        <>
+            <SideBarModules />
+            <Header setMenuModules={setMenuModules} menuModules={menuModules} />
+
+        </>
     )
 }
 
